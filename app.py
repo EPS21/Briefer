c9a474eb0dd52f84c545cc7f948c5c96b28b1426
Eric Sheng <ericsheng21@gmail.com>
2019-03-29 10:34:55 -0700

    initial commit

--- app.py
+++ app.py
@@ -6,10 +6,55 @@ __author__ = 'eps21'
 app = Flask(__name__)
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+print(APP_ROOT)
 
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])
 def index():
-    return render_template("briefer.html")
+    if request.method == 'POST':
+        target = os.path.join(APP_ROOT, 'uploads/')
+        print(target)
+
+        if not os.path.isdir(target):
+            os.mkdir(target)
+
+        for file in request.files.getlist("file"):
+            print(file)
+            filename = file.filename
+            destination = "/".join([target, filename])
+            print(destination)
+            file.save(destination)
+
+        msg = filename
+        return render_template("briefer.html", msg = msg)
+
+
+    elif request.method == 'GET':
+        msg = 'file not uploaded'
+        return render_template("briefer.html", msg = msg)
+
+
+    # return render_template("complete.html")
+    # return '''You have uploaded {}'''.format(filename)
+    # return jsonify(filename = filename)
+    #return render_template("briefer.html", msg=msg)
+
+@app.route('/jinja_test')
+@app.route('/jinja_test/<name>')
+def jinja_test(name='Johnny'):
+    if name == 'Johnny':
+        name = 'jonnnny'
+    elif name == 'Eric':
+        name = 'eeerrrriic'
+    return render_template('hello.html', name=name)
+
+@app.route('/read_test')
+def read_test():
+    # directory of application uploads folder
+    target = os.path.join(APP_ROOT, 'uploads/')
+    text = open(target+'sample-paragraph.txt', 'r+') #wtf is r+ for?
+    content = text.read()
+    text.close()
+    return render_template('readtest.html', text = content)
 
 
 @app.route('/add_numbers')
@@ -19,6 +64,7 @@ def add_numbers():
     b = request.args.get('b', 0, type=int)
     return jsonify(result = a + b)
 
+
 @app.route('/analyze_text_area')
 def analyze():
     textvar = request.args.get('text', 0, type=str)
@@ -26,10 +72,12 @@ def analyze():
     return jsonify(textresult = textvar + append)
 
 
-@app.route("/upload", methods=["POST"])
+# example to upload files
+@app.route("/upload", methods=['POST'])
 def upload():
-    target = os.path.join(APP_ROOT, 'images/')
+    target = os.path.join(APP_ROOT, 'uploads/')
     print(target)
+
 
     if not os.path.isdir(target):
         os.mkdir(target)
@@ -41,7 +89,62 @@ def upload():
         print(destination)
         file.save(destination)
 
-    return render_template("complete.html")
+    msg = filename
 
+    #return render_template("complete.html")
+    #return '''You have uploaded {}'''.format(filename)
+    # return jsonify(filename = filename)
+    return render_template("briefer.html", msg = msg)
+
+
+###################
+# The other video #
+###################
+
+@app.route('/query_example')
+def query_example():
+    language = request.args.get('language')
+    return '<h1>The language is: {}</h1>'.format(language)
+
+@app.route('/form_example', methods=['POST', 'GET'])
+def form_example():
+    #basically like [HTTP post], if the method is post will return the post method here
+    if request.method == 'POST':
+        language = request.form.get('language')
+        framework = request.form['framework']
+        return '<h1>The language is {}. The framework is {}</h1>'.format(language, framework)
+
+    return '''<form method="POST">
+    Language <input type="text" name="language">
+    Framework <input type="text" name="framework">
+    <input type="submit">
+    </form>
+    '''
+
+# getting data from json
+@app.route('/json_example', methods=['POST'])
+def json_example():
+    req_data = request.get_json()
+
+    # handle a case where if a json value is empty
+    language = None
+    if 'language' in req_data:
+        language = req_data['language']
+
+    language = req_data['language']
+    framework = req_data['framework']
+    python_version = req_data['version_info']['python']
+    form_example = req_data['examples'][0]
+    boolean_test = req_data['boolean_test']
+
+    return '''<h1>
+    The language value is {}.
+    The framework value is {}.
+    The python version is {}.
+    The example at 0 index is {}.
+    The boolean value is {}.
+    </h1>'''.format(language, framework, python_version, form_example, boolean_test)
+
+# driver code
 if __name__ == "__main__":
     app.run(port=4555, debug=True)
--- static/custom.js
+++ static/custom.js
@@ -1,28 +1,8 @@
 // Text Area Detect Entities button function
-$( document ).ready(function() {
-    console.log( "ready!" );
+var myFileName = '';
 
-//    $("#text-area-btn").on("click", function() {
-//      var text = $("textarea").val();
-//      alert(text);
-//      $.ajax({
-//        type: "POST",
-//        url: "/hello", // url to flask detect entities method
-//        data: text,
-//        success: function (json) {
-//          // on success we get the json object
-//          alert('success');
-//        },
-//        error: function() {
-//          alert('failed');
-//          $("#result-section").html(`
-//            <div class="container">
-//              <p>This is some container para after the button section, using the jquery after method thing</p>
-//            </div>
-//          `);
-//        }
-//      });
-//    });
+$( document ).ready(function() {
+    console.log( "ready!!" );
 
     // Detect entities from uploaded base
     // maybe we'll want to get a result, after hitting a submit file btn?
@@ -48,6 +28,7 @@ $( document ).ready(function() {
       });
     });
 
+    // add numbers script
     $(function() {
         $('a#calculate').on('click', function() {
           $.getJSON($SCRIPT_ROOT + '/add_numbers', {
@@ -60,9 +41,9 @@ $( document ).ready(function() {
         });
     });
 
+    // get textarea text pass into analyze text area function
     $(function() {
         $('#text-area-btn').on('click', function() {
-            console.log($("textarea").val());
             $.getJSON($SCRIPT_ROOT + '/analyze_text_area', {
                 text: $("textarea").val()
               }, function(data) {
@@ -73,9 +54,38 @@ $( document ).ready(function() {
                   `);
               });
               return false;
-
         });
     });
+
+    // get file name string from filename
+    $('input[type="file"]').change(function(e){
+        var fileName = e.target.files[0].name;
+        myFileName = fileName;
+        alert('The file "' + fileName +  '" has been selected.');
+        console.log(myFileName)
+    });
+
+//    $(function() {
+//        $('#upload-form').change(function() {
+//            $.post($SCRIPT_ROOT + '/upload', function(data) {
+//                $("#result").html( data );
+//            })
+//        })
+//    })
+
+
+    // file upload function after user selects a file
+//    $(function() {
+//        $('#upload').change(function() {
+//            alert($('#upload').val());
+//            $.getJSON($SCRIPT_ROOT + '/upload', {
+//                filename: $('#upload').val()
+//              }, function(data) {
+//                    $("#result").text(data.filename);
+//              });
+//              return false;
+//        });
+//    });
 
 }); // end of document ready
 
